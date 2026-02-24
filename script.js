c63695932dc7c9554462663b0a991b5b5a115f3e
mdarfan97 <mdarfanrahul215@gmail.com>
2026-02-24 21:38:04 +0800

    first three button updated

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-console.log(document.body);
-let arr=[1,5,3,5,6];
-const filtermethod = arr.filter(i=>i==5)
-console.log(filtermethod);
+let interviewlist = [];
+let rejectlist = [];
+
+
+let total = document.getElementById('total');
+let interviewcount = document.getElementById('interviewcount');
+let rejectcount = document.getElementById('rejectcount');
+
+const allcardsection = document.getElementById('allcard');
+const maincontainer = document.querySelector('main');
+
+const allfilterbtn = document.getElementById('allfilterbtn');
+const interviewfilterbtn = document.getElementById('interviewfilterbtn');
+const rejectfilterbtn = document.getElementById('rejectfilterbtn');
+
+
+
+function calculatecount()
+{
+    total.innerText = allcardsection.children.length;
+    interviewcount.innerText = interviewlist.children.length;
+    rejectcount.innerText = rejectlist.children.length;
+}
+calculatecount()
+
+function toggleStyle(id){
+    allfilterbtn.classList.remove('bg-blue-500','text-white')
+    interviewfilterbtn.classList.remove('bg-blue-500','text-white')
+    rejectfilterbtn.classList.remove('bg-blue-500','text-white')
+
+    allfilterbtn.classList.add('bg-slate-100','text-black')
+    interviewfilterbtn.classList.add('bg-slate-100','text-black')
+    rejectfilterbtn.classList.add('bg-slate-100','text-black')
+    
+    const selected = document.getElementById(id)
+    selected.classList.remove('bg-slate-100','text-black');
+    selected.classList.add('bg-blue-500','text-white')
+}
